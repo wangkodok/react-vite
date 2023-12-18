350fa57ce32cc77a6f07c5fa1df4d83bfc489356
wangkodok <wangkodok@gmail.com>
2023-12-18 20:51:45 +0900

    이벤트 리스너 추가

--- vite-project/src/components/PostList.jsx
+++ vite-project/src/components/PostList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post.jsx";
+import NewPost from "./NewPost.jsx";
 import classes from "./PostList.module.css";
 
 function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="홍길동" body="남자입니다." />
-      <Post author="홍길순" body="여자입니다." />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="홍길동" body="남자입니다." />
+        <Post author="홍길순" body="여자입니다." />
+      </ul>
+    </>
   );
 }
 
