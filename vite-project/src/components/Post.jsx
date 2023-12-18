a76fcf598693bfd067058ce2d491526770b1fe95
wangkodok <wangkodok@gmail.com>
2023-12-18 19:10:00 +0900

    동적인 값 출력

--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -1,7 +1,10 @@
+const names = ["홍길동", "홍길순"];
+
 function Post() {
+  const resultTitle = Math.random() > 0.5 ? names[0] : names[1];
   return (
     <div>
-      <h1>제목</h1>
+      <h1>{resultTitle}</h1>
       <p>설명</p>
     </div>
   );
