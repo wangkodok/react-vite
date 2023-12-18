8118f135d7b2d9ff8911f02a7695f4456d729db5
wangkodok <wangkodok@gmail.com>
2023-12-18 19:20:33 +0900

    컴포넌트 재사용

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,15 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </main>
+  );
 }
 
 export default App;
