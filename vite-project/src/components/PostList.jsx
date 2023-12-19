1ab86002ba7f5ef386429538dcda5aa252cc52c9
wangkodok <wangkodok@gmail.com>
2023-12-19 21:22:09 +0900

    목록 데이터 표시

--- vite-project/src/components/PostList.jsx
+++ vite-project/src/components/PostList.jsx
@@ -18,9 +18,19 @@ function PostList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author="홍길순" body="여자입니다." />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "#2f2fa1" }}>
+          <h2>"New Post" 버튼을 눌러서 작성하세요.</h2>
+          <p>감사합니다.</p>
+        </div>
+      )}
     </>
   );
 }
