a16d60bfc351fcd5e5e96a69348c1f67ab0bd981
wangkodok <wangkodok@gmail.com>
2023-12-19 18:28:20 +0900

    리액트 훅 useState 끌어올리기

--- vite-project/src/components/PostList.jsx
+++ vite-project/src/components/PostList.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import Post from "./Post.jsx";
 import NewPost from "./NewPost.jsx";
 import classes from "./PostList.module.css";
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="홍길동" body="남자입니다." />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="홍길순" body="여자입니다." />
       </ul>
     </>
