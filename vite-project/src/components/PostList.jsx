936d33fca57da4d662e9a44ca23a919c915f74db
wangkodok <wangkodok@gmail.com>
2023-12-19 20:44:18 +0900

    양식 제출 처리

--- vite-project/src/components/PostList.jsx
+++ vite-project/src/components/PostList.jsx
@@ -5,30 +5,14 @@ import Modal from "./Modal.jsx";
 import classes from "./PostList.module.css";
 
 function PostList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="홍길순" body="여자입니다." />
       </ul>
     </>
