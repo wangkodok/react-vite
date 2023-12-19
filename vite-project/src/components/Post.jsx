55f83c21f667c493e961c02829f8a91925bbdbdb
wangkodok <wangkodok@gmail.com>
2023-12-19 19:28:37 +0900

    리액트 조건문

--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -1,11 +1,11 @@
 import classes from "./Post.module.css";
 
-function Post(props) {
+function Post({ author, body }) {
   return (
     <li className="post">
       <div style={{ color: "#2f2fa1" }} className={classes.post}>
-        <h1 className={classes.author}>{props.author}</h1>
-        <p className={classes.body}>{props.body}</p>
+        <h1 className={classes.author}>{author}</h1>
+        <p className={classes.body}>{body}</p>
       </div>
     </li>
   );
--- vite-project/src/components/PostList.jsx
+++ vite-project/src/components/PostList.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import Post from "./Post.jsx";
 import NewPost from "./NewPost.jsx";
+import Modal from "./Modal.jsx";
 import classes from "./PostList.module.css";
 
 function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -15,12 +21,28 @@ function PostList() {
     setEnteredAuthor(event.target.value);
   }
 
+  // let modalContent;
+  // if (modalIsVisible) {
+  //   modalContent = (
+  //     <Modal onClose={hideModalHandler}>
+  //       <NewPost
+  //         onBodyChange={bodyChangeHandler}
+  //         onAuthorChange={authorChangeHandler}
+  //       />
+  //     </Modal>
+  //   );
+  // }
+
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="홍길순" body="여자입니다." />
