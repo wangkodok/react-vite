9dfbb40b1d67dd6ce9a72841bd3944846b3eaef7
wangkodok <wangkodok@gmail.com>
2023-12-19 20:59:11 +0900

    목록 상태 관리

--- vite-project/src/components/PostList.jsx
+++ vite-project/src/components/PostList.jsx
@@ -5,11 +5,17 @@ import Modal from "./Modal.jsx";
 import classes from "./PostList.module.css";
 
 function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
       <ul className={classes.posts}>
