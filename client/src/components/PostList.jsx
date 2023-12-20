d1f00acb645a5b8b284541d9e24fa24569f5cb03
wangkodok <wangkodok@gmail.com>
2023-12-20 18:21:37 +0900

    로딩 인디케이터

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -6,12 +6,19 @@ import classes from "./PostList.module.css";
 
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
-      const response = await fetch("http://localhost:8080/posts");
-      const resData = await response.json();
-      setPosts(resData.posts);
+      setIsFetching(true);
+      try {
+        const response = await fetch("http://localhost:8080/posts");
+        const resData = await response.json();
+        setPosts(resData.posts);
+      } catch (error) {
+        console.log(error);
+      }
+      setIsFetching(false);
     }
     fetchPosts();
   }, []);
@@ -34,17 +41,22 @@ function PostList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "#2f2fa1" }}>
           <h2>"New Post" 버튼을 눌러서 작성하세요.</h2>
           <p>감사합니다.</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "#2f2fa1" }}>
+          <p>로딩하는 중...</p>
         </div>
       )}
     </>
